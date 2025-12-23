ad66841652d415e56b06d5f560bcce70f170570d
Omkar <omkarp800@gmail.com>
2025-12-23 18:03:34 +0530

    Moved JsonDataProvider.java class to test folder as scope was test in pom.xml

--- src/main/java/pageObjects/CartPage.java
+++ src/main/java/pageObjects/CartPage.java
@@ -1,15 +1,10 @@
 package pageObjects;
 
 import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 
 import utils.AbstractComponents;
 
